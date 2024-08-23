20ee68579ecdf69015e8b1377dfbbda3e773230d
VikramNagamanickam <nagamanickamvikram@mail.com>
2024-08-23 13:12:07 +0530

    Add project files.

---
+++ MyDataAccessLayer/CricketersDetailsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MyDataAccessLayer
+{
+    public class CricketersDetailsRepository
+    {
+        string connectionString = "server=DESKTOP-BLBGEHJ\\SQLEXPRESS;database=Cricketerlist;user Id=sa;password=Anaiyaan@123;";
+        SqlConnection con = null;
+
+        public CricketersDetailsRepository()
+        {
+            con = new SqlConnection(connectionString);
+        }
+      
+
+        
+    }
+}
---
+++ MyDataAccessLayer/Entity/CricketDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDataAccessLayer.Entity
+{
+    class CricketDetails
+    {
+        public long CricketersID { get; set; }
+
+        public long CricketerName { get; set; }
+
+        public long Totalinnings { get; set; }
+
+        public long TotalScore { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public long NumberOfFifties { get; set; }
+
+        public long NumberOfHundreds { get; set; }
+
+        public long HighestScore { get; set; }
+    }
+}
